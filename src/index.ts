c1d1a474e3988dabe6d5af405224fdcb75b46b2c
不守规矩的汪汪王 <2165842569@qq.com>
2023-10-15 17:17:27 +0800

    fix: semantic-release is added

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export function helloWorld() {
+    const message = 'Hello World,wzh';
+    return message;
+  }
+  
+  export function goodBye() {
+    const message = 'Goodbye,wzh';
+    return message;
+  }
+  
+  export default {
+    helloWorld,
+    goodBye,
+  };
